d55f196fe16d508ac7219708debc639fd664a823
rafaelsoares07 <rafaelsoares1936@gmail.com>
2025-05-07 08:36:21 -0300

    refactor: refatora o controller auth para ter apenas a responsabilidade de um controller

--- src/main/java/dev/rafael/cadastro/auth/AuthService.java
+++ src/main/java/dev/rafael/cadastro/auth/AuthService.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
